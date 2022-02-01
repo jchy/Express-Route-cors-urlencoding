de1edcfb0805028b80191b53d473a81cc3af372a
jchy <recjaswantcse@gmail.com>
2022-02-01 23:08:43 +0530

    Introduction to express js

---
+++ day_1/api/users/users.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const uuid=require('uuid');
+const Users=require('./User');
+const router=express.Router();
+
+router.get("/",(req,res)=>{
+    res.send(Users);
+})
+
+router.get("/:userid",(req,res)=>{
+    // console.log(user);
+    // const user = Users.find(user=> user.id===req.params.userid);
+    const user = Users.find(user => {
+       return (Number(user.id) === Number(req.params.userid));
+    });
+   
+    if(!user){
+        res.status(400).send("User not found");
+    }
+    res.status(200).json(user);
+})
+
+router.post("/",(req,res)=>{
+    // * Create a new user
+    try{
+        const {name, active}=req.body;
+        if(!name) throw new Error("Name is required");
+        if(!active) throw new Error("Active is required");
+        const user={
+            id: uuid.v4(),
+            name,
+            active
+        }
+        Users.push(user);
+        res.status(201).json(user);
+    }
+    catch(err){
+      res.status(400).send(`Invalid request: ${err.toString()}`)
+    }
+})
+
+module.exports = router;
+
